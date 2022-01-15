66b0e8b00d7cb2c7eea18e5846bf14c26cf07269
Bao Nguyen <86527891+quocbao19982009@users.noreply.github.com>
2022-01-15 14:57:37 +0200

    app finish v1

--- src/screens/FeeCalculartor.tsx
+++ src/screens/FeeCalculartor.tsx
@@ -1,107 +1,85 @@
 import React, { useState } from "react";
-import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import {
+  Form,
+  Button,
+  Container,
+  Row,
+  Col,
+  Popover,
+  OverlayTrigger,
+} from "react-bootstrap";
+import { deliveryCostCal } from "../ultilities/deliveryCostCal";
+import classes from "./FeeCalculartor.module.css";
 
 const FeeCalculartor = () => {
   const today = new Date();
-
   const [cartValue, setCartValue] = useState<number>(0);
   const [distance, setDistance] = useState<number>(0);
   const [items, setItems] = useState<number>(0);
   const [deliveryPrice, setDeliveryPrice] = useState<number>(0);
   const [date, setDate] = useState<Date>(today);
 
-  // Friday  date.getDay() = 5
-  //   console.log(date.getDay());
-  //   3PM - 7PM
-  //   console.log(date.getHours());
-
-  let totalDeliveryFree = 0;
-
-  console.log("BEGIN");
-
-  let surcharge = 0;
-
-  const calculatingMethod = () => {
-    //   base Delivery cost = 2
-    // Handle Cart Value surcharge
-    if (cartValue < 10) {
-      surcharge = surcharge + 10 - +cartValue;
-    }
-    console.log("after Cart Value", surcharge);
-    // Handle Delivery Distance
-    const extraDistance = (distance - 1000) / 1000;
-    console.log("extraDistance km", extraDistance);
-
-    let deliveryCost = 2;
-    if (extraDistance > 0) {
-      if (extraDistance % 0.5 === 0) {
-        deliveryCost = deliveryCost + extraDistance / 0.5;
-      } else {
-        deliveryCost = deliveryCost + Math.trunc(extraDistance / 0.5) + 1;
-      }
-      console.log("deliveryCost", deliveryCost);
-    }
-
-    surcharge = surcharge + deliveryCost;
-
-    // Handle Order Items
-    if (items >= 5) {
-      let extraItems = 0.5 * (items - 5);
-      surcharge = extraItems + surcharge;
-      console.log("Extra Items cost", extraItems);
-    }
-
-    // console.log("friday", date.getDay() === 5);
-    // console.log("Time", date.getUTCHours());
-    // console.log(
-    //   "after 3Pm",
-    //   +date.toLocaleString("en-US", { hour: "numeric", hour12: false })
-    // );
-    // console.log(
-    //   "before 7Pm",
-    //   +date.toLocaleString("en-US", { hour: "numeric", hour12: false }) < 19
-    // );
-
-    if (
-      date.getDay() === 5 &&
-      +date.toLocaleString("en-US", { hour: "numeric", hour12: false }) > 15 &&
-      +date.toLocaleString("en-US", { hour: "numeric", hour12: false }) < 19
-    ) {
-      surcharge = surcharge * 1.1;
-    }
-
-    if (surcharge > 15) {
-      surcharge = 15;
-    }
-
-    if (cartValue >= 100) {
-      surcharge = 0;
-    }
-  };
+  console.log("Begin");
 
   const submitHandler = (e: React.FormEvent) => {
     e.preventDefault();
-    calculatingMethod();
-    setDeliveryPrice(surcharge);
+    const toltalCost = deliveryCostCal(
+      cartValue,
+      distance,
+      items,
+      date
+    ).toFixed(2);
+    setDeliveryPrice(Number(toltalCost));
+    console.log("calculate");
   };
+
+  const resetHandler = () => {
+    setCartValue(0);
+    setDistance(0);
+    setItems(0);
+    setDeliveryPrice(0);
+    setDate(today);
+  };
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Header as="h3">Cart Value</Popover.Header>
+      <Popover.Body>
+        We recommend you buy more then 10e for the lowest deilivery cost. Order
+        more then 100e is free delivery
+      </Popover.Body>
+    </Popover>
+  );
+
+  const Example = () => (
+    <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+      <Button variant="success">Click me to see</Button>
+    </OverlayTrigger>
+  );
 
   return (
     <Container>
-      <Form
-        className="mt-5 text-algin-center"
-        onSubmit={(e) => submitHandler(e)}
-      >
+      <header className={classes.header}>
+        <h1>WOLT CALCULATOR</h1>
+      </header>
+
+      <Form className={classes.form} onSubmit={(e) => submitHandler(e)}>
         <Form.Group className="mb-3" controlId="cartValue">
-          <Form.Label>Cart Value (€) </Form.Label>
+          <OverlayTrigger trigger="hover" placement="right" overlay={popover}>
+            <Form.Label>Cart Value (€) </Form.Label>
+          </OverlayTrigger>
 
           <Form.Control
             className="text-algin-center"
             type="number"
-            placeholder="20"
+            placeholder="Cart value"
             onChange={(e) => {
               setCartValue(+e.target.value);
             }}
             step="any"
+            min="0"
+            required={true}
+            value={cartValue}
           />
         </Form.Group>
 
@@ -109,10 +87,13 @@ const FeeCalculartor = () => {
           <Form.Label>Delivery Distance (m)</Form.Label>
           <Form.Control
             type="number"
-            placeholder="900"
+            placeholder="Distance"
+            min="0"
+            required={true}
             onChange={(e) => {
               setDistance(+e.target.value);
             }}
+            value={distance}
           />
         </Form.Group>
 
@@ -120,10 +101,14 @@ const FeeCalculartor = () => {
           <Form.Label>Amount of Items </Form.Label>
           <Form.Control
             type="number"
-            placeholder="1"
+            placeholder="Items"
             onChange={(e) => {
               setItems(+e.target.value);
             }}
+            required={true}
+            min="0"
+            step="1"
+            value={items}
           />
         </Form.Group>
 
@@ -131,19 +116,26 @@ const FeeCalculartor = () => {
           <Form.Label>Time</Form.Label>
           <Form.Control
             type="datetime-local"
-            placeholder="today"
             onChange={(e) => {
               const date = new Date(e.target.value);
               setDate(date);
             }}
+            required={true}
           />
         </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Calculate Delivery Price
-        </Button>
+        <div className={classes.functions}>
+          <h2 className={classes.total}>Deliver Price: {deliveryPrice}€ </h2>
+          <div className={classes.buttons}>
+            <Button variant="primary" type="submit" className={classes.button}>
+              Calculate Delivery Price
+            </Button>
+            <Button variant="primary" type="reset" className={classes.button}>
+              Reset
+            </Button>
+          </div>
+        </div>
       </Form>
-      <h2>Deliver Price: {deliveryPrice.toFixed(2)}</h2>
     </Container>
   );
 };
---
+++ src/ultilities/deliveryCostCal.ts
@@ -0,0 +1,126 @@
+enum FridayRushHour {
+  Friday = 5,
+  Start3PM = 15,
+  End7Pm = 19,
+}
+
+export const deliveryCostCal = (
+  cartValue: number,
+  distance: number,
+  items: number,
+  date: Date
+): number => {
+  let totalDeliveryCost = 0;
+  const minimumCartValue = 10;
+  const defaultDeliveryFee = 2;
+  const addtionalDeliveryCost = 1 / 500;
+  const additionItemsNumberCost = 0.5;
+  const maximumSurcharge = 15;
+  const maximumCartValue = 100;
+  const rushHourCost = 1.1;
+
+  // CartValue > 100
+  if (cartValue >= maximumCartValue) {
+    totalDeliveryCost = 0;
+    return totalDeliveryCost;
+  }
+
+  // if Cart Value < 10e
+  if (cartValue < minimumCartValue) {
+    totalDeliveryCost = totalDeliveryCost + minimumCartValue - cartValue;
+  }
+
+  // Delivery Fee Cost
+  let deliveryFeeCost: number = defaultDeliveryFee;
+
+  const extraDistance = distance - 1000;
+  if (extraDistance > 0) {
+    if (extraDistance % 500 === 0) {
+      deliveryFeeCost =
+        defaultDeliveryFee + extraDistance * addtionalDeliveryCost;
+    } else {
+      deliveryFeeCost =
+        defaultDeliveryFee +
+        Math.trunc(extraDistance * addtionalDeliveryCost + 1);
+    }
+  }
+  totalDeliveryCost = totalDeliveryCost + deliveryFeeCost;
+
+  // Surchage for Items Number
+  let surchageItemsNumber: number;
+
+  if (items > 5) {
+    surchageItemsNumber = additionItemsNumberCost * (items - 5);
+    totalDeliveryCost = totalDeliveryCost + surchageItemsNumber;
+  }
+
+  // Friday Rush Hours
+  const selectedTime = +date.toLocaleString("en-US", {
+    hour: "numeric",
+    hour12: false,
+  });
+
+  if (
+    date.getDay() === FridayRushHour.Friday &&
+    selectedTime >= FridayRushHour.Start3PM &&
+    selectedTime <= FridayRushHour.End7Pm
+  ) {
+    totalDeliveryCost = totalDeliveryCost * rushHourCost;
+  }
+
+  // Special Case
+
+  // Delivery Cost > 15
+  if (totalDeliveryCost > maximumSurcharge) {
+    totalDeliveryCost = maximumSurcharge;
+  }
+
+  return totalDeliveryCost;
+};
+
+// const calculatingMethod = () => {
+//   //   base Delivery cost = 2
+//   // Handle Cart Value surcharge
+//   if (cartValue < 10) {
+//     surcharge = surcharge + 10 - +cartValue;
+//   }
+//   console.log("after Cart Value", surcharge);
+//   // Handle Delivery Distance
+//   const extraDistance = (distance - 1000) / 1000;
+//   console.log("extraDistance km", extraDistance);
+
+//   let deliveryCost = 2;
+//   if (extraDistance > 0) {
+//     if (extraDistance % 0.5 === 0) {
+//       deliveryCost = deliveryCost + extraDistance / 0.5;
+//     } else {
+//       deliveryCost = deliveryCost + Math.trunc(extraDistance / 0.5) + 1;
+//     }
+//     console.log("deliveryCost", deliveryCost);
+//   }
+
+//   surcharge = surcharge + deliveryCost;
+
+//   // Handle Order Items
+//   if (items >= 5) {
+//     let extraItems = 0.5 * (items - 5);
+//     surcharge = extraItems + surcharge;
+//     console.log("Extra Items cost", extraItems);
+//   }
+
+//   if (
+//     date.getDay() === 5 &&
+//     +date.toLocaleString("en-US", { hour: "numeric", hour12: false }) > 15 &&
+//     +date.toLocaleString("en-US", { hour: "numeric", hour12: false }) < 19
+//   ) {
+//     surcharge = surcharge * 1.1;
+//   }
+
+//   if (surcharge > 15) {
+//     surcharge = 15;
+//   }
+
+//   if (cartValue >= 100) {
+//     surcharge = 0;
+//   }
+// };
